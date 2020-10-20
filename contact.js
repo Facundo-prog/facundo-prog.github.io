08e28d2b28cd56804588442842654e007ffbc98a
facucarroz <facucarroz@gmail.com>
2020-10-20 10:57:43 -0300

    JS update

--- contact.js
+++ contact.js
@@ -1,21 +1,17 @@
 
-
 function ajustarTamaño(){
     let menu = document.getElementById("menu");
     let titulo = document.getElementById("titulo");
     let sub = document.getElementById("subtitulo");
-    let texto = document.getElementById("texto");
 
     if(menu.clientWidth < 600){
         menu.style = "font-size:5vw;";
         titulo.style = "font-size:6vw;";
         sub.style = "font-size:4vw;";
-        texto.style = "font-size:4vw;";
     }
     else{
         menu.style = "font-size:20px;";
         titulo.style = "font-size:30px;";
         sub.style = "font-size:18px;";
-        texto.style = "font-size:16px;";
     }
 }
