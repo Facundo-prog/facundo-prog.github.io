b81741a0f858d73c6227f9141313c0ba4d0c29a6
facucarroz <facucarroz@gmail.com>
2020-10-25 20:38:48 -0300

    Update

--- inicio/index.js
+++ inicio/index.js
@@ -16,12 +16,14 @@ function ajustarTamaño(){
 }
 
 function mostrarMenuLateral(){
+    document.body.style = "font-size:18px;";
     cont_img.style = "display:block;";
     lista.style = "display:none;";
     lista_lateral.style = "display:none;"
 }
 
 function esconderMenuLateral(){
+    document.body.style = "font-size:;";
     cont_img.style = "display:none;";
     lista.style = "display:block;";
     lista_lateral.style = "display:none;"
