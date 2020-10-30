a3c68bba4fb87e11f043dfd35f902d4170e60b6a
facucarroz <facucarroz@gmail.com>
2020-10-29 22:35:23 -0300

    Update

--- inicio/index.js
+++ inicio/index.js
@@ -1,5 +1,5 @@
 const size = new autoAdjustSize(1000, "cont_img", "lista", "lista_lateral");
-const repos = new repository("guillermo-gerard", 8);
+const repos = new repository("guillermo-gerard", 8);//quantityGetRepos 0 = todos
 
 
 function getArrayRepos(quantityReturnRepos){
@@ -13,54 +13,3 @@ function getArrayRepos(quantityReturnRepos){
     }
     request.send();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const repos = new repository("guillermo-gerard");
-const size = new autoAdjustSize(1000, "cont_img", "lista", "lista_lateral");
-var reposOrdenados = [];
-var user = [];
-
-function getRepos(){
-    repos.getRepos();
-    setTimeout(listRepos,1000);
-}
-
-
-function openLink(id){
-    let host = reposOrdenados[id][2];
-    window.location = host;
-}
-
-function listRepos(){
-
-    if(user.length > 0){
-        reposOrdenados = repos.lastRepo(user);
-        showRepos(8);
-    }
-    else{
-        alert("Ups! El repositorio solisitado no esta disponible");
-    }
-}
-
-function showRepos(quantity){
-    repos.showRepos(quantity,(user.length-1),"cont_proyecto","texto_proyecto","desc_proyecto","proyecto");
-}
-*/
--- proyectos/proyectos.js
+++ proyectos/proyectos.js
@@ -1,15 +1,15 @@
-const s = new autoAdjustSize(1000, "cont_img", "lista", "lista_lateral");
-const r = new repository("guillermo-gerard", 0);//quantityGetRepos 0 = todos
+const size = new autoAdjustSize(1000, "cont_img", "lista", "lista_lateral");
+const repos = new repository("guillermo-gerard", 0);//quantityGetRepos 0 = todos
 
 
 function getArrayRepos(quantityReturnRepos){
     let request = new XMLHttpRequest();
 
-    request.open("GET", "https://api.github.com/users/" + r.user + "/repos", true);
+    request.open("GET", "https://api.github.com/users/" + repos.user + "/repos", true);
     request.onload = () =>{ 
-        r.setArrayUser(JSON.parse(request.responseText)); 
-        r.lastRepo();
-        r.showRepos(quantityReturnRepos,"cont_proyecto","texto_proyecto","desc_proyecto","proyecto");
+        repos.setArrayUser(JSON.parse(request.responseText)); 
+        repos.lastRepo();
+        repos.showRepos(quantityReturnRepos,"cont_proyecto","texto_proyecto","desc_proyecto","proyecto");
     }
     request.send();
 }
