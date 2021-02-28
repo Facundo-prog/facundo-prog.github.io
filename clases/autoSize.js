cbb9af78ef38359876bfba307a5a6284f4e96bdb
facucarroz <facucarroz@gmail.com>
2021-02-28 00:55:10 -0300

    javascript updated

--- clases/autoSize.js
+++ clases/autoSize.js
@@ -4,22 +4,22 @@ class autoAdjustSize{
         this.size = size;
         this.cont_img = document.getElementById(id_img);
         this.lista = document.getElementById(id_lista);
-        this.lista_lateral = document.getElementById(id_lista_lateral);
         this.estadoPrecionado = false;
     }
 
     ajustarTamaño(){
 
         this.estadoPrecionado = false;
-        this.lista_lateral.style = "display:none;"
     
         if(document.body.clientWidth <= this.size){
-            this.cont_img.style = "display:block;";
             this.lista.style = "display:none;";
+            this.cont_img.style = "display:block;";
+            this.lista.className = "lista_lateral";
         }
         else{
-            this.cont_img.style = "display:none;";
             this.lista.style = "display:block;";
+            this.cont_img.style = "display:none;";
+            this.lista.className = "lista";
         }
     }
 
@@ -28,10 +28,10 @@ class autoAdjustSize{
         this.estadoPrecionado = !this.estadoPrecionado;
     
         if(this.estadoPrecionado == false){
-            this.lista_lateral.style = "display:none;"
+            this.lista.style = "display:none;";
         }
         else{
-            this.lista_lateral.style = "display:block;"
+            this.lista.style = "display:block;";
         }
     }
 }
